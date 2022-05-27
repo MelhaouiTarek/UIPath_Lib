6db45f8b3228b2cffe7010c01980cf5326b92bec
MelhaouiTarek <melhaoui.tarek@gmail.com>
2022-05-27 18:08:56 +0100

    Add project files.

---
+++ Company.Product1/Company.Product1/CustomException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Company.Product1
+{
+    class CustomException : Exception
+    {
+        private int Code;
+
+        private CustomException()
+            : base()
+        {
+            Code = -1;
+        }
+        public CustomException(String Message)
+        : base(Message)
+        {
+            Code = -1;
+        }
+        public CustomException(String Message, int Code)
+            : base(Message)
+        {
+            this.Code = Code;
+        }
+
+    }
+}
